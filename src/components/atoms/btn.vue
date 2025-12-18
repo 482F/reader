f8202c408b1d521c76d404a1eade127fab87a8d8
482F <482F.git.mail@482f.net>
2025-12-19 00:00:00 +0900

    ボタンコンポーネント追加

---
+++ src/components/atoms/btn.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="btn" @click="emits('click')">
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+const emits = defineEmits<{ (e: 'click'): void }>()
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  border: solid 1px lightgray;
+  border-radius: 4px;
+  background-color: #00000000;
+  &:hover,
+  &:focus {
+    background-color: #00000006;
+  }
+}
+</style>
